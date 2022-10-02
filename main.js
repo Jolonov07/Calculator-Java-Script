a18df3fff7916de85184c80b7888a41c1f14dd56
Jolonov07 <zolotoy.baiaman@gmail.com>
2022-10-02 17:42:21 +0600

    Простой калькулятор на JavaScript

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
-const input = document.querySelector('.input')
-const buttons = document.querySelectorAll('button')
-const equals = document.querySelector('.equals')
+const input = document.querySelector('.input') //input
+const buttons = document.querySelectorAll('button') //all butttons
+const equals = document.querySelector('.equals') // =
 let buttonText = ''
 let value = ''
 
-equals.addEventListener('click', () => {
-	if (input.value === '') {
-		input.value = ' '
-	}
-})
 buttons.forEach(item => {
 	item.addEventListener('click', e => {
 		buttonText = e.target.innerText
@@ -27,52 +22,3 @@ buttons.forEach(item => {
 		}
 	})
 })
-
-// ====================================================================== //
-
-// const input  = document.querySelector('.input')
-// const left = document.querySelector('.left') // (
-// const right = document.querySelector('.right') // )
-// const clear = document.querySelector('.clear') // clear
-// const percent = document.querySelector('.percent') //%
-
-// const zero = document.querySelector('.zero')
-// const one = document.querySelector('.one')
-// const two = document.querySelector('.two')
-// const three = document.querySelector('.three')
-// const four = document.querySelector('four')
-// const five = document.querySelector('.five')
-// const six = document.querySelector('.six')
-// const seven = document.querySelector('.seven')
-// const eight = document.querySelector('.eight')
-// const nine = document.querySelector('.nine')
-
-// const plus = document.querySelector('.plus') //+
-// const minus = document.querySelector('.minus') //-
-// const x = document.querySelector('.x') //*
-// const division = document.querySelector('.division') // /
-
-// const dot = document.querySelector('.dot') //.
-// const equals = document.querySelector('.equals') //=
-
-// let screen = document.querySelector('.input')// buttons = document.querySelectorAll('button')
-// let screenValue = ''
-
-// for (item of buttons) {
-// 	item.addEventListener('click', e => {
-// 	let	buttonText = e.target.innerText
-// 		if (buttonText == 'X') {
-// 			buttonText = '*'
-// 			screenValue += buttonText
-// 			screen.value = screenValue
-// 		} else if (buttonText == 'C') {
-// 			screenValue = ''
-// 			screen.value = screenValue
-// 		} else if (buttonText == '=') {
-// 			screen.value = eval(screenValue)
-// 		} else {
-// 			screenValue += buttonText
-// 			screen.value = screenValue
-// 		}
-// 	})
-// }
